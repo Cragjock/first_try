ccb7074a711516b1249b7c1b83c117e601f7c440
Cragjock <steve.clark@dda.kyocera.com>
2016-05-31 22:54:38 -0700

    Create pitime.h

---
+++ pitime.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <math.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+//static void error(char*);
+//static int open_socket(char* host, char* port);
+//static int open_socket2(char* host, char* port);
+//static int say(int socket, char* s);
+
+char* pitime();
+char* alt_pitime(); 
